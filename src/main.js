fbd9478624d5db727724f8c7e2ae480f2e830f41
Vlad Averin <vlad@orbitum.co>
2021-10-05 14:40:45 -0600

    Add Demo section with embedded demo video

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import VueYouTubeEmbed from 'vue-youtube-embed'
 import './styles/index.css'
 
 Vue.config.productionTip = false
 require('dotenv').config()
+
+Vue.use(VueYouTubeEmbed)
 
 new Vue({
   store,
