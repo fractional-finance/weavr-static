3753510661217dbb63bea313173f8dcc51e8bd72
sunosuporno <sarkarsuporno36@gmail.com>
2022-05-31 20:08:21 +0530

    edited dropdown alignment

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -22,7 +22,7 @@
             x-transition:leave="transition ease-in duration-75"
             x-transition:leave-start="transform opacity-100 scale-100"
             x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48"
+            class="absolute -left-7 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48"
           >
             <div class="px-2 py-2 bg-[#1e1c25] rounded-md shadow">
               <a
@@ -48,7 +48,7 @@
             x-transition:leave="transition ease-in duration-75"
             x-transition:leave-start="transform opacity-100 scale-100"
             x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48"
+            class="absolute -left-16 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48"
           >
             <div class="px-2 py-2 bg-[#1e1c25] rounded-md shadow">
               <a
@@ -76,7 +76,7 @@
             x-transition:leave="transition ease-in duration-75"
             x-transition:leave-start="transform opacity-100 scale-100"
             x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48"
+            class="absolute -left-16 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48"
           >
             <div class="px-2 py-2 bg-[#1e1c25] rounded-md shadow">
               <a
