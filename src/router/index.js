4c8877d0f82856ed083f47c867c7cb9433f723e4
zeryx <1892175+zeryx@users.noreply.github.com>
2022-06-11 09:17:15 -0300

    burntcoat typeform redirect added

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import NotFound from '../views/NotFound.vue'
 import Wip from '../views/Wip.vue'
+import Wrapper from "../views/Wrapper.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,7 +14,16 @@ const router = createRouter({
     {
       path: '/wip',
       name: "Work In Progress",
-      component: Wip    },
+      component: Wip
+    },
+    {
+      path: '/burntcoat',
+      component: Wrapper,
+      name: 'Welcome to your first real estate token',
+      beforeEnter(to, from, next){
+        window.location.href="https://dkwnhnualcm.typeform.com/to/QObnRTbh";
+      }
+    },
       
     { path: '/:pathMatch(.*)*', name: 'Not-Found', component: NotFound }
   ]
