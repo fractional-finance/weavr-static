31550f351677115a707eace3fd0dfc638b4215e5
Tony Stark <afemiadom@icloud.com>
2022-09-30 11:29:13 -0500

    Added a gate page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import NotFound from '../views/NotFound.vue'
 import Wip from '../views/Wip.vue'
+import Gate from '../views/Gate.vue'
 import Wrapper from "../views/Wrapper.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/wip',
       name: "Work In Progress",
       component: Wip
+    },
+    {
+      path: '/gate',
+      name: "Gate",
+      component: Gate
     },
       
     { path: '/:pathMatch(.*)*', name: 'Not-Found', component: NotFound }
---
+++ src/views/Gate.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class>
+        <div
+            class="w-5/6 m-auto mt-6 xl:mt-20 flex flex-col xl:flex-row xl:justify-between items-center font-body"
+        >
+            <div class="flex flex-col items-center xl:items-start xl:w-2/5">
+                <p
+                    class="text-3xl md:text-5xl xl:text-7xl text-center xl:text-left font-heading font-semibold"
+                >
+                    Buy, Sell
+                    <br />and
+                    <span class="text-[#5A50D8]">Build</span>
+                </p>
+                <p
+                    class="mt-5 md:mt-6 xl:mt-10 xl:w-3/4 md:text-xl text-center xl:text-left text-sm text-zinc-400"
+                >
+                    The first totally decentralized Real estate market. Invest in
+                    tokenized Real Estate, Manage it, earn, and sell anytime.
+                </p>
+                <div
+                    class="flex flex-row justify-between items-center mt-5 xl:mt-9 text-sm md:text-lg"
+                >
+                    <a
+                        href="#"
+                        class="px-3 md:px-5 py-1.5 md:py-3 bg-[#5A50D8] rounded-sm mr-3"
+                    >Authenticate</a>
+                </div>
+            </div>
+            <img src="../assets/hero-image.svg" alt class="mt-5 w-3/4 md:w-3/5 xl:w-2/5" />
+        </div>
+
+        <div
+            class="w-5/6 m-auto mt-5 md:mt-8 px-3.5 md:px-5 py-3 md:py-4 md:flex md:flex-col xl:flex-row md:items-center xl:justify-center"
+        >
+            <p>Private Members Club, not open to the public</p>
+        </div>
+
+        <div
+            class="w-5/6 m-auto mt-5 md:mt-8 font-body bg-white text-black px-3.5 md:px-5 py-3 md:py-4 md:flex md:flex-col xl:flex-row md:items-center xl:justify-center rounded-lg"
+        >
+            <div class="xl:flex xl:flex-col xl:items-start xl:ml-14">
+                <p
+                    class="text-2xl md:text-5xl xl:text-3xl font-semibold text-center xl:text-left"
+                >Gain Access to our Marketplace</p>
+                <p
+                    class="mt-3 md:mt-7 md:text-xl xl:text-lg md:text-center xl:text-left"
+                >Join our newsletter and learn to gain your access to Frabric</p>
+                <div class="flex flex-row justify-evenly mt-3 md:mt-6 text-sm">
+                    <a
+                        href="https://3a567f7a.sibforms.com/serve/MUIEAAkS53zR0h7rhxExKB1X1JotCHaM4o7FBy9qRH3V7h92zXfDojr8PpuqQVRtRIYoeLgWddoOz_IVgW3DofxtESXPIwfentMntmPtVWwU_sTQTpYSt6ESaeVmSheGFH20mDIsMgM1UbzffXQ9ghnsJl8wKxqmH0Lxs7_Ppe1Kd6CVHsywoa4e0jFJBCru4UTyeIdV_z53X2yd"
+                        class="px-5 md:px-7 py-2 md:py-3 bg-[#5A50D8] rounded-sm md:rounded-md text-white md:text-xl xl:text-sm"
+                        style="z-index: 999;"
+                    >Subscribe</a>
+                </div>
+            </div>
+            <img
+                src="../assets/envelope.svg"
+                alt="Subscribe to our newsletter"
+                class="ml-14 md:ml-40 xl:ml-36 xl:-mr-28 -mt-6 md:-mt-20 xl:-mt-28"
+            />
+        </div>
+        <!--   <iframe width="580" height="525" src="https://3a567f7a.sibforms.com/serve/MUIEAGQx7T-wtvSq_ZgppYcoGL_EhMhib4X8SjJhbe5tKHAT3T8_-VssK2tpbB45mIhFBfhwkgW1LiIouyQD03GEUoL4VR7x4U9JXWFhdBiTWBUa5IR6mTOZIWxnv1964sjZzI_swXwut-7QTeTnMtvrMnePnzvOVXB-_XyX3_729sjz94iV2JjJdw51x7u5WvwA2ELLJIP9R2G1" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>-->
+    </div>
+</template>
+
+<style></style>
